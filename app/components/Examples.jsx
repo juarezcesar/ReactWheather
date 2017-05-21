e59878c5cc6b9734a4988a969751e82ae697f91b
Juarez Cesar Filho <juarez.cesar@devmaker.com.br>
2017-05-21 18:09:28 -0300

    Added copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,16 +1,12 @@
 var React = require('react');
 
-// var Examples = React.createClass({
-//   render: function(){
-//     return(
-//       <h3>Examples Component</h3>
-//     )
-//   }
-// });
 
 var Examples = (props) => {
   return(
-    <h3>Examples Component</h3>
+    <div>
+      <h3>Examples Component!</h3>
+      <p>Welcome</p>
+    </div>
   )
 }
 
