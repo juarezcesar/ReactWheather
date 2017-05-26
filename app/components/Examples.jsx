8070f2ceaeecf233fc279cb13871a95ae20bacca
Juarez Cesar Filho <juarez.cesar@devmaker.com.br>
2017-05-26 11:19:57 -0300

    Some examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,14 +1,23 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 
 var Examples = (props) => {
   return(
     <div>
-      <h3>Examples</h3>
-      <p>Welcome</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few examples to try out</p>
+      <ol>
+        <li>
+          <Link to='/?location=Philadelphia'>Philadelphia, PA</Link>
+        </li>
+        <li>
+          <Link to='/?location=New York'>New York, NY</Link>
+        </li>
+      </ol>
     </div>
   )
-}
+};
 
 
 module.exports =  Examples;
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,6 +1,6 @@
 var React = require('react');
 var Nav = require('Nav')
-// 
+//
 // var Main = React.createClass({
 //   render: function(){
 //     return(
@@ -17,8 +17,12 @@ var Main = (props) => {
   return(
     <div>
       <Nav/>
-      <h2>Main Component</h2>
-      {props.children}
+      <div className="row">
+        <div className="columns medium-6 larger-4 small-centered">
+          {props.children}
+        </div>
+      </div>
+            
     </div>
   );
 }
